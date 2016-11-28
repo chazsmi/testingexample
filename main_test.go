3306c8d4ade9ed40d9d8bd8fd6780009ec7388b3
Charie Smith <charlie.smith1@sainsburys.co.uk>
2016-11-28 14:50:12 +0000

    Add 1.7 test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,10 @@ package main
 // Your tests are done per package they can be incudled in the same package
 // or added in seperate package depending on what your are doung
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // Naming convenation for tests are Test followed by the function/method you are testing
 func TestAverage(t *testing.T) {
@@ -32,6 +35,29 @@ func TestTableAverage(t *testing.T) {
 				tc.A, tc.B, actual, tc.Expected,
 			)
 		}
+	}
+}
+
+func TestTableOneSevenAverage(t *testing.T) {
+	// Create a slice of a stuct with ints
+	cases := []struct{ A, B, Expected float64 }{
+		{1, 2, 1.5},
+		{4, 23, 13.5},
+		{455, 6667, 3561},
+	}
+	// Run each case
+	for _, tc := range cases {
+		// The run method allows you run each test in turn and get a fail output
+		// for a particluar test
+		t.Run(fmt.Sprintf("%f and %f", tc.A, tc.B), func(t *testing.T) {
+			actual := Average([]float64{tc.A, tc.B})
+			if actual != tc.Expected {
+				t.Errorf(
+					"got %f, expected %f",
+					tc.A, tc.B, actual, tc.Expected,
+				)
+			}
+		})
 	}
 }
 
